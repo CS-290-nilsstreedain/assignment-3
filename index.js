5d4fea6f8f88beb19d3285cd329224e694a32033
Nils Streedain <nils@streedain.com>
2022-11-03 00:25:26 -0700

    Added initial create post

--- index.js
+++ index.js
@@ -11,5 +11,49 @@ function toggleSellModal() {
 	document.querySelectorAll("input").forEach(input => input.value = "");
 }
 
+function createPost() {
+	var post = Object.assign(document.createElement("div"), {className: "post"});
+	post.dataset.price = document.getElementById("post-price-input").value;
+	post.dataset.city = document.getElementById("post-city-input").value;
+	post.dataset.condition = document.querySelector('input[name="post-condition"]:checked').getAttribute("id").split('-')[2];
+	
+	var postContents = Object.assign(document.createElement("div"), {className: "post-conents"});
+	post.appendChild(postContents);
+	
+	var postImageContainer = Object.assign(document.createElement("div"), {className: "post-image-container"});
+	postContents.appendChild(postImageContainer);
+	
+	postImageContainer.appendChild(Object.assign(document.createElement("img"), {
+		src: document.getElementById("post-photo-input").value,
+		alt: document.getElementById("post-text-input").value
+	}));
+	
+	var postInfoContainer = Object.assign(document.createElement("div"), {className: "post-info-container"});
+	postContents.appendChild(postInfoContainer);
+	
+	postInfoContainer.appendChild(Object.assign(document.createElement("a"), {
+		className: "post-title",
+		href: "#",
+		textContent: document.getElementById("post-text-input").value
+	}));
+	
+	postInfoContainer.appendChild(Object.assign(document.createElement("span"), {
+		className: "post-price",
+		textContent: document.getElementById("post-price-input").value
+	}));
+	
+	postInfoContainer.appendChild(Object.assign(document.createElement("span"), {
+		className: "post-city",
+		textContent: `(${document.getElementById("post-city-input").value})`
+	}));
+	
+	posts.appendChild(post);
+	
+	toggleSellModal();
+}
+
 document.getElementById("sell-something-button").addEventListener("click", toggleSellModal);
 document.getElementById("modal-close").addEventListener("click", toggleSellModal);
+document.getElementById("modal-cancel").addEventListener("click", toggleSellModal);
+
+document.getElementById("modal-accept").addEventListener("click", createPost);
