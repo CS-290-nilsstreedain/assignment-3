629d08a899c54c7360837bfade6cbff9bb882324
Nils Streedain <nils@streedain.com>
2022-11-02 22:03:36 -0700

    Added Modal toggle & clear

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
  * Write your client-side JS code in this file.  Don't forget to include your
  * name and @oregonstate.edu email address below.
  *
- * Name:
- * Email:
+ * Name: Nils Streedain
+ * Email: streedan@oregonstate.edu
  */
+function toggleSellModal() {
+	document.getElementById("sell-something-modal").classList.toggle("hidden");
+	document.getElementById("modal-backdrop").classList.toggle("hidden");
+	document.querySelectorAll("input").forEach(input => input.value = "");
+}
+
+document.getElementById("sell-something-button").addEventListener("click", toggleSellModal);
+document.getElementById("modal-close").addEventListener("click", toggleSellModal);
